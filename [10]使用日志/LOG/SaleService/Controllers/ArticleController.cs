2786aaefd67b8f3a86084eb624bf7cb1a4cfda5d
GaoHaichun <gao.haichun@ztesoft.com>
2018-07-09 16:01:22 +0800

    logger

---
+++ [10]使用日志/LOG/SaleService/Controllers/ArticleController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SaleService.Models;
+
+namespace SaleService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Article")]
+    public class ArticleController : Controller
+    {
+        private readonly SalesContext _context;
+        private readonly ILogger _logger;
+
+        public ArticleController(SalesContext context, ILogger<ArticleController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+       
+
+        [HttpGet("logger")]
+        public void TestLogger()
+        {
+            Console.WriteLine("===========TestLogger==============");
+           
+            _logger.LogCritical("LogCritical");
+            _logger.LogError("LogError");          
+            _logger.LogWarning("LogWarning");
+            _logger.LogInformation("LogInformation");
+            _logger.LogDebug("LogDebug");
+
+            return;
+        }
+    }
+}
---
+++ [10]使用日志/LOG/SaleService/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleService.Models
+{
+    public class User
+    {
+        [Key]
+        public string UserID { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public int State { get; set; }
+    }
+}
